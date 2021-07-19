6cb96b577bc8660046cd2c2a2e73692a7febd1b0
Justin Kim <justin.kim3420@gamil.com>
2021-07-19 18:17:58 -0400

    uibuild

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const cors = require('cors')
 
 const app = express()
 
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static('build'))
+}
+
 app.use(express.json())
 app.use(cors())
 
