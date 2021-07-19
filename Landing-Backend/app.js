5aca9e2a3058c9e32f8ac76e49ea6e090eca4a06
Justin Kim <justin.kim3420@gmail.com>
2021-07-19 15:10:23 -0400

    starting the addition of DB

--- Landing-Backend/app.js
+++ Landing-Backend/app.js
@@ -1,11 +1,31 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
+
+const connect = (databaseURL) => {
+    if (process.env.ENVIRONMENT === 'test') {
+        await mongoose.connect(databaseURL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+        });
+    }
+}
+
+const disconnect = () => {
+
+}
+
+const clearDatabase = () => {
+
+}
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-module.exports={
+module.exports = {
     app
 }
